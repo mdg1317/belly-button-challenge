d7f7e0d1a9092e09e515ccaeb3999706f1a36bb4
mdg1317 <matthew.dennis137@gmail.com>
2024-03-27 17:37:08 -0700

    Initialized data with page load, edited charts

--- static/js/app.js
+++ static/js/app.js
@@ -3,26 +3,34 @@ var jsonData;
 d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then(function(data){jsonData = init(data);});
 
 function init(data){
-    //console.log(data);
+    console.log(data);
 
     // Add names to dropdown list
     for(let i = 0; i < data.names.length; i++){
         d3.select("#selDataset").append("option").text(data.names[i]);
     }
 
+    // Initialize starting dataset
+    optionChanged(data, d3.select("#selDataset").property("value"));
+
     return data;
 }
 
-function optionChanged(value){
+function optionChanged(data, value){
     // Get metadata and samples of selected id
     // find() function taken from StackOverflow: https://stackoverflow.com/questions/7364150/find-object-by-id-in-an-array-of-javascript-objects
-    let metadata = jsonData.metadata.find(item => {
+    let metadata = data.metadata.find(item => {
         return parseInt(item.id) === parseInt(value);
     });
-    let samples = jsonData.samples.find(item => {
+    let samples = data.samples.find(item => {
         return parseInt(item.id) === parseInt(value);
     });
 
+    setDemographicInfo(metadata);
+    createCharts(samples);
+}
+
+function setDemographicInfo(metadata){
     // Set info in Demographic Info table
     d3.select("#info-id").text(`id: ${metadata.id}`);
     d3.select("#info-ethnicity").text(`ethnicity: ${metadata.ethnicity}`);
@@ -31,8 +39,6 @@ function optionChanged(value){
     d3.select("#info-location").text(`location: ${metadata.location}`);
     d3.select("#info-bbtype").text(`bbtype: ${metadata.bbtype}`);
     d3.select("#info-wfreq").text(`wfreq: ${metadata.wfreq}`);
-
-    createCharts(samples);
 }
 
 function createCharts(samples){
@@ -51,14 +57,25 @@ function createCharts(samples){
         return b.sample_value - a.sample_value;
     });
 
-    // Create trace for horizontal bar chart
-    let dataBar = [{
+    // Create traces for all charts
+    let traceArr = [[{
         x: sortedSamples.map(sample => sample.sample_value).slice(0, 10).reverse(),
         y: sortedSamples.map(sample => `OTU ${sample.otu_id}`).slice(0, 10).reverse(),
         type: "bar",
-        orientation: "h"
-    }];
+        orientation: "h",
+        text: sortedSamples.map(sample => sample.otu_label).slice(0, 10).reverse()
+    }],[{
+        x: samples.otu_ids,
+        y: samples.sample_values,
+        mode: 'markers',
+        marker: {
+          size: samples.sample_values,
+          color: samples.otu_ids
+        },
+        text: samples.otu_labels
+    }]];
     
     // Plot the charts
-    Plotly.newPlot("bar", dataBar);
+    Plotly.newPlot("bar", traceArr[0]);
+    Plotly.newPlot("bubble", traceArr[1]);
 }
